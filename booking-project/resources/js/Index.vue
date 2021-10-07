1290cad021c21dd1b1164dead9139d7b3eef4eec
Kate Bryan <kate.bryan@and.digital>
2021-10-07 16:20:37 +0100

    feat: brought in basic styles

--- booking-project/resources/js/Index.vue
+++ booking-project/resources/js/Index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/test">Test</router-link>
         <router-view></router-view>
     </div>
 </template>;
