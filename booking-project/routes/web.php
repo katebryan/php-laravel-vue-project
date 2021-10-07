39f1069b0f0062bf72d6720ab407b719b13c55ef
Kate Bryan <kate.bryan@and.digital>
2021-10-07 10:10:17 +0100

    feat: added a fallback route

--- booking-project/routes/web.php
+++ booking-project/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// created a fallback route - any non `api` routes will redirect to here: 
+Route::get('/{any?}', function () {
     return view('welcome');
-});
+})->where('any', '^(?!api\/)[\/\w\.\,-]*');
