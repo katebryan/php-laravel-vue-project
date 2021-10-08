01696073048df3ae72fbfd0628dd5a72c77ea672
Kate Bryan <kate.bryan@and.digital>
2021-10-08 11:29:48 +0100

    chore: navbar styling using bootstrap

--- booking-project/resources/js/Index.vue
+++ booking-project/resources/js/Index.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/test">Test</router-link>
+        <nav class="navbar bg-white border-bottom navbar-light">
+        <router-link class="navbar-brand mr-auto" :to="{name: 'home'}">Laravel Booking Project</router-link>
+        <router-link class="btn nav-button" :to="{name: 'test'}">Test</router-link>
+        </nav>
+        <div class="container mt-4 mb-4 pr-4 pl-4">
         <router-view></router-view>
+        </div>
     </div>
 </template>;
