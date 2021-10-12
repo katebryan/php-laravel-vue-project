21b28a91da8ba9cc983ed3cd6ab71f3b6583d463
Kate Bryan <kate.bryan@and.digital>
2021-10-12 16:02:03 +0100

    feat: added new Resources

--- booking-project/app/Http/Controllers/Api/PropertyController.php
+++ booking-project/app/Http/Controllers/Api/PropertyController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Bookable;
+use App\Http\Resources\PropertyShowResource;
+use App\Http\Resources\PropertyIndexResource;
 
 class PropertyController extends Controller
 {
     public function index()
     {
-        return Bookable::all();
+        return PropertyIndexResource::collection(Bookable::all());
     }
 
     public function show($id)
     {
-        return Bookable::findOrFail($id);
+        return new PropertyShowResource(Bookable::findOrFail($id));
     }
 }
--- booking-project/app/Http/Resources/PropertyShowResource.php
+++ booking-project/app/Http/Resources/PropertyShowResource.php
@@ -14,6 +14,10 @@ class PropertyShowResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content
+        ];
     }
 }
