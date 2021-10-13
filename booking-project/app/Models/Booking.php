ddec443aa5866a1a74a5b83635dea12ecb5996e0
Kate Bryan <kate.bryan@and.digital>
2021-10-13 20:47:44 +0100

    fix: fixed Model bug

--- booking-project/app/Models/Booking.php
+++ booking-project/app/Models/Booking.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['from', 'to'];
+
     public function bookable()
     {
         return $this->belongsTo(Bookable::class);
--- booking-project/database/factories/BookingFactory.php
+++ booking-project/database/factories/BookingFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Booking;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
 class BookingFactory extends Factory
 {
@@ -21,8 +22,11 @@ class BookingFactory extends Factory
      */
     public function definition()
     {
+        $from = Carbon::instance($this->faker->dateTimeBetween('-1 months', '+1 months'));
+        $to = (clone $from)->addDays(random_int(0, 14));
         return [
-            //
+            'from' => $from,
+            'to' => $to
         ];
     }
 }
