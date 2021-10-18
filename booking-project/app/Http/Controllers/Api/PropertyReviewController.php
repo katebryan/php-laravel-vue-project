a161b7b5680e5f320e6bcdaf6a3e956ccf453750
Kate Bryan <kate.bryan@and.digital>
2021-10-18 15:10:45 +0100

    feat: added PropertyReviewIndexResource

--- booking-project/app/Http/Controllers/Api/PropertyReviewController.php
+++ booking-project/app/Http/Controllers/Api/PropertyReviewController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PropertyReviewIndexResource;
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class PropertyReviewController extends Controller
@@ -13,8 +15,10 @@ class PropertyReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
-        //
+        $bookable = Bookable::findOrFail($id);
+
+        return PropertyReviewIndexResource::collection($bookable->reviews()->latest()->get());
     }
 }
